7259c72f8bc2f631260d0281be81e05bf6aff6a7
rautha <32843705+rautha@users.noreply.github.com>
2021-04-16 12:27:13 +0200

    Add files via upload

---
+++ src/main/java/es/uji/ei1027/proyecto1027/Dao/ServiceRowMapper.java
@@ -0,0 +1,20 @@
+package es.uji.ei1027.clubesportiu.dao;
+
+
+import es.uji.ei1027.clubesportiu.model.Service;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public final class ServiceRowMapper implements RowMapper<Service> {
+    public Service mapRow(ResultSet rs, int rowNum) throws SQLException
+{
+Service service = new Service();
+service.setCode(rs.getString( "code" ));
+service.setType(rs.getString( "type" ));
+service.setDescription(rs.getString( "description" ));
+service.setInitial_Date(rs.getString( "initial_date" ));
+return service;
+}
+}
