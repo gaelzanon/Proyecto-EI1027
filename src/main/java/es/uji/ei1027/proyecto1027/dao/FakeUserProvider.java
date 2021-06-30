f6e1cb3ccae242512bbe93e69f45d317565d7f60
al386180 <al386180@uji.es>
2021-07-01 00:43:52 +0200

    Cambios en FakeUserProvider. De momento no cambia la funcionalidad

--- src/main/java/es/uji/ei1027/proyecto1027/dao/FakeUserProvider.java
+++ src/main/java/es/uji/ei1027/proyecto1027/dao/FakeUserProvider.java
@@ -2,28 +2,100 @@ package es.uji.ei1027.proyecto1027.dao;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import es.uji.ei1027.proyecto1027.model.*;
 import org.jasypt.util.password.BasicPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import es.uji.ei1027.proyecto1027.model.UserDetails;
+
+import javax.sql.DataSource;
 
 @Repository
 public class FakeUserProvider implements UserDao {
     final Map<String, UserDetails> knownUsers = new HashMap<String, UserDetails>();
 
+    private CitizenDao citizenDao;
+    private ControllerDao controllerDao;
+    private MunicipalityManagerDao municipalityManagerDao;
+    //private EnvironmentalManagerDao environmentalManagerDao;
+
+    @Autowired
+    public void setCitizenDao(CitizenDao citizenDao) {
+        this.citizenDao=citizenDao;
+    }
+    @Autowired
+    public void setControllerDao(ControllerDao controllerDao) {
+        this.controllerDao=controllerDao;
+    }
+    @Autowired
+    public void setMunicipalityManagerDao(MunicipalityManagerDao municipalityManagerDao) {
+        this.municipalityManagerDao=municipalityManagerDao;
+    }
+    /*@Autowired
+    public void setEnvironmentalManagerDao(EnvironmentalManagerDao environmentalManagerDao) {
+        this.environmentalManagerDao=environmentalManagerDao;
+    }*/
+
     public FakeUserProvider() {
         BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
+
+        List<Citizen> citizenList= citizenDao.getCitizens();
+        List<Controller> controllerList= controllerDao.getControllers();
+        List<MunicipalityManager> municipalityManagerList= municipalityManagerDao.getMunicipalityManagers();
+        //List<EnvironmentalManager> environmentalManagerList= environmentalManagerDao.getEnvironmentalManagers();
+
+        for(Citizen citizen:citizenList){
+            UserDetails userClient = new UserDetails();
+            userClient.setUsername(citizen.getName());
+            userClient.setPassword(passwordEncryptor.encryptPassword(citizen.getPassword()));
+            userClient.setNIF(citizen.getNIF());
+            userClient.setUserType(UserDetailsEnum.Citizen.toString());
+            knownUsers.put(userClient.getNIF(), userClient);
+        }
+
+        for(Controller controller:controllerList){
+            UserDetails userClient = new UserDetails();
+            userClient.setUsername(controller.getName());
+            userClient.setPassword(passwordEncryptor.encryptPassword(controller.getPassword()));
+            userClient.setNIF(controller.getNIF());
+            userClient.setUserType(UserDetailsEnum.Controller.toString());
+            knownUsers.put(userClient.getNIF(), userClient);
+        }
+
+        for(MunicipalityManager municipalityManager:municipalityManagerList){
+            UserDetails userClient = new UserDetails();
+            userClient.setUsername(municipalityManager.getName());
+            userClient.setPassword(passwordEncryptor.encryptPassword(municipalityManager.getPassword()));
+            userClient.setNIF(municipalityManager.getNIF());
+            userClient.setUserType(UserDetailsEnum.MunicipalManager.toString());
+            knownUsers.put(userClient.getNIF(), userClient);
+        }
+
+
+        /*for(EnvironmentalManager environmentalManager:environmentalManagerList){
+            UserDetails userClient = new UserDetails();
+            userClient.setUsername(environmentalManager.getName());
+            userClient.setPassword(passwordEncryptor.encryptPassword(environmentalManager.getPassword()));
+            userClient.setNIF(environmentalManager.getNIF());
+            userClient.setUserType(UserDetailsEnum.EnvironmentalManager.toString());
+            knownUsers.put(userClient.getNIF(), userClient);
+        }*/
+
+
         UserDetails userClient = new UserDetails();
         userClient.setUsername("citizen");
         userClient.setPassword(passwordEncryptor.encryptPassword("citizen"));
-        userClient.setUserType("Citizen");
+        userClient.setUserType(UserDetailsEnum.Citizen.toString());
         userClient.setNIF("00000000A");
         knownUsers.put("citizen", userClient);
 
         UserDetails userController = new UserDetails();
         userController.setUsername("controller");
         userController.setPassword(passwordEncryptor.encryptPassword("controller"));
-        userController.setUserType("Controller");
+        userController.setUserType(UserDetailsEnum.Controller.toString());
         userController.setNIF("11111111A");
         knownUsers.put("controller", userController);
 
@@ -37,7 +109,7 @@ public class FakeUserProvider implements UserDao {
         UserDetails userEnvironmental = new UserDetails();
         userEnvironmental.setUsername("environmental");
         userEnvironmental.setPassword(passwordEncryptor.encryptPassword("environmental"));
-        userEnvironmental.setUserType("EnvironmentalManager");
+        userEnvironmental.setUserType(UserDetailsEnum.EnvironmentalManager.toString());
         knownUsers.put("environmental", userEnvironmental);
 
         UserDetails userAdmin = new UserDetails();
