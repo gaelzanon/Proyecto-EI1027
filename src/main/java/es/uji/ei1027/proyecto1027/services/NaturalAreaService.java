5f97011b9cbf18f49e64dc75825acfbd45bcaa17
Gael Zanón <al288187@uji.es>
2021-07-04 16:59:19 +0200

    mas cosas

--- src/main/java/es/uji/ei1027/proyecto1027/services/NaturalAreaService.java
+++ src/main/java/es/uji/ei1027/proyecto1027/services/NaturalAreaService.java
@@ -1,9 +1,12 @@
 package es.uji.ei1027.proyecto1027.services;
 
+import es.uji.ei1027.proyecto1027.model.MunicipalityManager;
 import es.uji.ei1027.proyecto1027.model.Zone;
 
 import java.util.List;
 
 public interface NaturalAreaService {
     public List<Zone> getAllNatAreaZones(String code_area);
+    public String getCodigoMunicipalParaMunicipalManager(String nif);
+    public String getMunicipalityName(String codeMun);
 }
--- src/main/java/es/uji/ei1027/proyecto1027/services/NaturalAreaSvc.java
+++ src/main/java/es/uji/ei1027/proyecto1027/services/NaturalAreaSvc.java
@@ -1,6 +1,9 @@
 package es.uji.ei1027.proyecto1027.services;
 
+import es.uji.ei1027.proyecto1027.dao.MunicipalityDao;
+import es.uji.ei1027.proyecto1027.dao.MunicipalityManagerDao;
 import es.uji.ei1027.proyecto1027.dao.ZoneDao;
+import es.uji.ei1027.proyecto1027.model.MunicipalityManager;
 import es.uji.ei1027.proyecto1027.model.Zone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +16,24 @@ public class NaturalAreaSvc implements NaturalAreaService {
     @Autowired
     ZoneDao zoneDao;
 
+    @Autowired
+    MunicipalityDao municipalityDao;
+
+    @Autowired
+    MunicipalityManagerDao municipalityManagerDao;
+
     @Override
     public List<Zone> getAllNatAreaZones(String code_area) {
         return zoneDao.getZonesArea(code_area);
+    }
+
+    @Override
+    public String getCodigoMunicipalParaMunicipalManager(String nif) {
+        return municipalityDao.getMunicipalityCode(municipalityManagerDao.getMunicipalityManager(nif).getMunicipalityName());
+    }
+
+    @Override
+    public String getMunicipalityName(String codeMun) {
+        return municipalityDao.getMunicipality(codeMun).getName();
     }
 }
